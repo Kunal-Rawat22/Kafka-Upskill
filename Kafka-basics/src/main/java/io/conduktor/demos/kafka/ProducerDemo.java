4bf5864adc036810da8c3c92ae6b88b9584019a7
Kunal-Rawat-TTN <kunal.rawat@tothenew.com>
2026-03-24 00:19:31 +0530

    KFK-UP: Project Setup

--- Kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
+++ Kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -1,4 +1,12 @@
 package io.conduktor.demos.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class);
+
+    public static void main(String[] args) {
+        log.info("Starting ProducerDemo");
+    }
 }
