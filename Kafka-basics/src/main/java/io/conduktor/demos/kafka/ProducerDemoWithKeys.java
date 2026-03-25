de1fd432c0414c1b44991f2aeb8f6ee924940cc8
Kunal-Rawat-TTN <kunal.rawat@tothenew.com>
2026-03-25 05:59:34 +0530

    KFK-UP: Created Basic Producer with keys

--- Kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoWithKeys.java
+++ Kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoWithKeys.java
@@ -2,7 +2,6 @@ package io.conduktor.demos.kafka;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RoundRobinPartitioner;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,8 +9,8 @@ import org.slf4j.LoggerFactory;
 import java.util.Objects;
 import java.util.Properties;
 
-public class ProducerDemoWithCallbacks {
-    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallbacks.class);
+public class ProducerDemoWithKeys {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithKeys.class);
 
     public static void main(String[] args) {
         log.info("Starting ProducerDemo");
@@ -37,7 +36,7 @@ public class ProducerDemoWithCallbacks {
         // create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(producerProperties);
 
-        ProducerDemoWithCallbacks.sendMultipleMessages(producer);
+        ProducerDemoWithKeys.sendMultipleMessages(producer);
 
         // flush data - synchronous
         producer.flush();
@@ -47,20 +46,19 @@ public class ProducerDemoWithCallbacks {
     }
 
     public static void sendMultipleMessages(KafkaProducer<String, String> producer) {
-        // By Default, the producer will batch records together to improve efficiency. The "batch.size" property controls the maximum size of a batch in bytes. When the batch size is reached, the producer will send the batch of records to the broker. You can adjust this property based on your use case and the expected size of your messages. In this example, we set the batch size to 400 bytes.
-        // By Default, it uses sticky partitioning, which means that all messages with the same key will be sent to the same partition. If you want to use a different partitioning strategy, you can specify a custom partitioner by setting the "partitioner.class" property in the producer configuration. For example, you can use the RoundRobinPartitioner to distribute messages evenly across partitions.
-        for (int j = 0; j < 10; j++) {
-            for (int i = 0; i < 30; i++) {
+        for(int j = 0; j < 2; j++) {
+            for (int i = 0; i < 10; i++) {
 
                 // create a producer record
                 String topic = "demo_java";
-                ProducerRecord<String, String> record = new ProducerRecord<>(topic, "Hello World " + i);
+                String key = "id_" + i;
+                String value = "Hello World " + i;
+                ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
 
                 // send data - asynchronous
                 producer.send(record, (metadata, e) -> {
                     if (Objects.isNull(e)) {
-                        log.info("Received new metadata.\nTopic: {}\nPartition: {}\nOffset: {}\nTimestamp: {}",
-                                metadata.topic(), metadata.partition(), metadata.offset(), metadata.timestamp());
+                        log.info("Received new metadata. Key: {} | Partition: {}", key, metadata.partition());
                     } else {
                         log.error("Error while producing", e);
                     }
