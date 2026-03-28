81322bd61da8ce83e154a9c74a8279c26fa1473a
Kunal-Rawat-TTN <kunal.rawat@tothenew.com>
2026-03-28 09:41:17 +0530

    KFK-UP: Added Wikimedia Producer

--- kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangeHandler.java
+++ kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangeHandler.java
@@ -1,4 +1,45 @@
 package io.conduktor.demos.kafka.wikimedia;
 
-public class WikimediaChangeHandler {
+import com.launchdarkly.eventsource.MessageEvent;
+import com.launchdarkly.eventsource.background.BackgroundEventHandler;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WikimediaChangeHandler implements BackgroundEventHandler {
+    private KafkaProducer<String, String> producer;
+    private String topic;
+    private final Logger log = LoggerFactory.getLogger(WikimediaChangeHandler.class);
+
+    public WikimediaChangeHandler(KafkaProducer<String, String> producer, String topic) {
+        this.producer = producer;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen() {
+        // Nothing here
+    }
+
+    @Override
+    public void onClosed() {
+        producer.close();
+    }
+
+    @Override
+    public void onMessage(String s, MessageEvent messageEvent) {
+        log.info("Wikimedia change handler received event: {}", messageEvent.getData());
+        producer.send(new ProducerRecord<>(topic, messageEvent.getData()));
+    }
+
+    @Override
+    public void onComment(String s) {
+        // nothing here
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        log.error("Wikimedia change handler received error: {}", throwable.getMessage());
+    }
 }
--- kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -1,4 +1,64 @@
 package io.conduktor.demos.kafka.wikimedia;
 
+import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.HttpConnectStrategy;
+import com.launchdarkly.eventsource.background.BackgroundEventHandler;
+import com.launchdarkly.eventsource.background.BackgroundEventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+import java.util.Properties;
+
 public class WikimediaChangesProducer {
+    private static final Logger log = LoggerFactory.getLogger(WikimediaChangesProducer.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        log.info("Starting WikimediaChangesProducer");
+
+        String bootstrapServers = "localhost:9092";
+        String topic = "wikimedia.recentchange";
+        String eventUrl = "https://stream.wikimedia.org/v2/stream/recentchange";
+        Properties producerProperties = new Properties();
+        producerProperties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        // set producer properties
+        producerProperties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProperties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProperties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, "400");
+
+        // Setting for safe Kafka Producers (Kafka <= 2.8)
+        producerProperties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+//        producerProperties.setProperty("min.insync.replicas", "2");
+        producerProperties.setProperty(ProducerConfig.RETRIES_CONFIG, String.valueOf(Integer.MAX_VALUE));
+        producerProperties.setProperty(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, String.valueOf(120000));
+        producerProperties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+        producerProperties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(producerProperties);
+
+        BackgroundEventSource eventSource = buildEventHandler(producer, topic, eventUrl);
+//        BackgroundEventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+//        EventSource.Builder eventSourceBuilder = new EventSource.Builder(URI.create(eventUrl));
+//        BackgroundEventSource.Builder builder = new BackgroundEventSource.Builder(eventHandler, eventSourceBuilder);
+//        BackgroundEventSource eventSource = builder.build();
+        eventSource.start();
+
+        Thread.sleep(60000);
+    }
+
+    private static BackgroundEventSource buildEventHandler(final KafkaProducer<String, String> producer, String topic, String eventSourceUrl) {
+        BackgroundEventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+
+        //Build connect strategy with headers
+        HttpConnectStrategy connectStrategy = HttpConnectStrategy.http(URI.create(eventSourceUrl)).header("User-Agent","my-kafka-producer/1.0(contact: admin@ourcompany.com)");
+
+        EventSource.Builder eventSourceBuilder = new EventSource.Builder(connectStrategy);
+
+        return new BackgroundEventSource.Builder(eventHandler, eventSourceBuilder).build();
+    }
 }
