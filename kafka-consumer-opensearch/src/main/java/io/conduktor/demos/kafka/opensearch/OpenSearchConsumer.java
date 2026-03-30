1046d499be6befbec7d779536f791a5bcc47eac7
Kunal-Rawat-TTN <kunal.rawat@tothenew.com>
2026-03-30 13:57:16 +0530

    KFK-UP: Added Bulk ingestion instead of single ingestion

--- kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -1,5 +1,8 @@
 package io.conduktor.demos.kafka.opensearch;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.gson.JsonParser;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -11,6 +14,8 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.opensearch.action.bulk.BulkRequest;
+import org.opensearch.action.bulk.BulkResponse;
 import org.opensearch.action.index.IndexRequest;
 import org.opensearch.action.index.IndexResponse;
 import org.opensearch.client.RequestOptions;
@@ -69,30 +74,64 @@ public class OpenSearchConsumer {
         RestHighLevelClient openSearchClient = createOpenSearchClient();
 
         KafkaConsumer<String, String> kafkaConsumer = createKafkaConsumer();
+        ObjectMapper objectMapper = new ObjectMapper();
 
         // We need to create index if it doesn't exist already
-        try(openSearchClient; kafkaConsumer) {
+        try (openSearchClient; kafkaConsumer) {
             boolean indexExists = openSearchClient.indices().exists(new GetIndexRequest(INDEX_NAME), RequestOptions.DEFAULT);
             if (!indexExists) {
                 CreateIndexRequest createIndexRequest = new CreateIndexRequest(INDEX_NAME);
                 openSearchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
                 log.info("Created index: {}", INDEX_NAME);
-            }
-            else log.info("Index already exists");
+            } else log.info("Index already exists");
 
             kafkaConsumer.subscribe(List.of("wikimedia.recentchange"));
-            while(true)
-            {
+            while (true) {
                 ConsumerRecords<String, String> records = kafkaConsumer.poll(java.time.Duration.ofMillis(3000));
                 log.info("Received {} records", records.count());
+
+                BulkRequest bulkRequest = new BulkRequest();
                 for (ConsumerRecord<String, String> record : records) {
-                    IndexRequest indexRequest = new IndexRequest(INDEX_NAME).source(record.value(), XContentType.JSON);
-                    IndexResponse indexResponse = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
-                    log.info("Indexed record: {}", indexResponse.getId());
+                    // Strategy 1: Define an ID using Kafka record coordinates to make our consumer idempotent
+                    // String id = record.topic() + "_" + record.partition() + "_" + record.offset();
+
+                    // Strategy 2: Extract an ID from the JSON value and use it as document ID in OpenSearch to make our consumer idempotent
+                    String id = extractIdFromJson(record.value());
+
+                    // Parse JSON
+                    ObjectNode jsonNode = (ObjectNode) objectMapper.readTree(record.value());
+
+                    // Remove problematic field
+                    jsonNode.remove("log_params");
+
+                    log.info("Record Id: {}, record: {}", id, record.value());
+                    IndexRequest indexRequest = new IndexRequest(INDEX_NAME).source(jsonNode.toString(), XContentType.JSON).id(id);
+                    bulkRequest.add(indexRequest);
+//                    IndexResponse indexResponse = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+//                    log.info("Indexed record: {}", indexResponse.getId());
+                }
+
+                if (bulkRequest.numberOfActions() > 0) {
+                    BulkResponse bulkResponse = openSearchClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+                    log.info("Indexed {} records", bulkResponse.getItems().length);
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        log.error("Error while sleeping", e);
+                    }
+
+                    // commit offsets after every batch
+                    kafkaConsumer.commitSync();
+                    log.info("Committed {} records", records.count());
                 }
             }
         }
 
+    }
+
+    public static String extractIdFromJson(String json) {
+        return JsonParser.parseString(json).getAsJsonObject().get("meta").getAsJsonObject().get("id").getAsString();
     }
 
     public static KafkaConsumer<String, String> createKafkaConsumer() {
@@ -108,6 +147,7 @@ public class OpenSearchConsumer {
         consumerProperties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         consumerProperties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         consumerProperties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, group_id);
+        consumerProperties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 
         //If a consumer already has committed offsets, earliest does nothing
         consumerProperties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
